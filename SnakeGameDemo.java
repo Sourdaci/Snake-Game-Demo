25ef360886132c8d2c7acaa8cd729416c8b1635f
Sourdaci <sourdaci@gmail.com>
2015-03-12 19:24:58 +0100

    Clase Snake nueva, separada de Clase Demo

--- SnakeGameDemo.java
+++ SnakeGameDemo.java
@@ -10,170 +10,43 @@ import java.awt.Color;
  */
 public class SnakeGameDemo
 {
-    // instance variables - replace the example below with your own
-    private Canvas pantalla;
-    private Pen serpiente;
-    private Random aleatorio;
-    private ArrayList<SnakePosition> posiciones;
-    private static final int INICIO_X = 200, INICIO_Y = 200;
-    private static final int TRAZO = 20;
-    private static final int[] GIRO = {-90, 0, 90, 180};
+    private Snake gameDemo;
 
     /**
-     * Constructor for objects of class SnakeGameDemo
+     * Crea un juego Snake para pruebas
      */
     public SnakeGameDemo()
     {
-        aleatorio = new Random();
-        pantalla = new Canvas("Snake Demo", (INICIO_X * 2), (INICIO_Y * 2));
-        reinicio();
+        gameDemo = new Snake();
     }
     
     /**
-     * Inicia los elementos del juego
-     * Si se ha iniciado un juego, se reiniciaran
-     */
-    private void reinicio(){
-        pantalla.erase();
-        serpiente = new Pen(INICIO_X, INICIO_Y, pantalla);
-        posiciones = new ArrayList<SnakePosition>();
-        posiciones.add(new SnakePosition(INICIO_X, INICIO_Y));
-        serpiente.turn(GIRO[aleatorio.nextInt(GIRO.length)]);
-    }
-    
-    /**
-     * Dibuja la serpiente inicial de 3 segmentos
-     * Es inicio de partida, reinicia la pantalla y la serpiente
+     * Inicia la partida de prueba
      */
     public void drawSnake(){
-        int contador = 0;
-        reinicio();
-        do{
-            int rotar = aleatorio.nextInt(GIRO.length - 1);
-            serpiente.turn(GIRO[rotar]);
-            serpiente.move(TRAZO);
-            posiciones.add(new SnakePosition(serpiente.getPositionX(), serpiente.getPositionY()));
-            contador++;
-        }while (contador < 3);
+        gameDemo.drawSnake();
     }
     
     /**
-     * Dibuja otro segmento de la serpiente
+     * Hace crecer 1 segmento la serpiente de prueba
      */
     public void makeSnakeBigger(){
-        if(posiciones.size() > 1){
-            ArrayList<Integer> giroTemp = new ArrayList<Integer>();
-            boolean dibujado = false;
-            SnakePosition ultima = posiciones.get(posiciones.size() - 1);
-            for (int i = 0; i< GIRO.length - 1; i++){
-                giroTemp.add(GIRO[i]);
-            }
-            do{
-                int nuevoGiro = aleatorio.nextInt(giroTemp.size());
-                serpiente.penUp();
-                serpiente.turn(giroTemp.get(nuevoGiro));
-                serpiente.move(TRAZO);
-                int posX = serpiente.getPositionX();
-                int posY = serpiente.getPositionY();
-                boolean colision = false;
-                for (int ind = 0; ind < posiciones.size() && !colision; ind++){
-                    SnakePosition temporal = posiciones.get(ind);
-                    if(temporal.getPosX() == posX && temporal.getPosY() == posY 
-                    || posX <= 0 || posY <= 0 || posX >= (INICIO_X * 2) || posY >= (INICIO_Y * 2)){
-                        colision = true;
-                    }
-                }
-                serpiente.moveTo(ultima.getPosX(), ultima.getPosY());
-                serpiente.penDown();
-                if(!colision){
-                    serpiente.move(TRAZO);
-                    int x = serpiente.getPositionX();
-                    int y = serpiente.getPositionY();
-                    posiciones.add(new SnakePosition(x, y));
-                    dibujado = true;
-                }else{
-                    int corregir = giroTemp.get(nuevoGiro);
-                    serpiente.turn( -corregir);
-                    giroTemp.remove(nuevoGiro);
-                }
-            }while(giroTemp.size() > 0 && !dibujado);
-            if(!dibujado){
-                System.out.println("No quedan movimientos, juego acabado.");
-            }
-        }
+        gameDemo.makeSnakeBigger();
     }
     
     /**
-     * Dibuja varios fragmentos de Serpiente
-     * El numero debe ser mayor de 0, o no hara nada
+     * Hace crecer varios segmentos la serpiente de prueba
      * 
-     * @param cantidad El numero de trazos de serpiente a dibujar
+     * @param veces La cantidad de segmentos que debe crecer la serpiente de prueba
      */
-    public void makeSnakeBigger(int cantidad){
-        if(cantidad > 0){
-            for(int i=0; i < cantidad; i++){
-                makeSnakeBigger();
-            }
-        }
+    public void makeSnakeBigger(int veces){
+        gameDemo.makeSnakeBigger(veces);
     }
     
     /**
-     * Mueve la serpiente por la pantalla
-     * Si no pudiera moverse, informa de ello en el juego
+     * Hace moverse 1 vez a la serpiente de prueba
      */
     public void animateSnake(){
-        if(posiciones.size() > 1){
-            ArrayList<Integer> giroTemp = new ArrayList<Integer>();
-            boolean dibujado = false;
-            SnakePosition ultima = posiciones.get(posiciones.size() - 1);
-            for (int i = 0; i< GIRO.length - 1; i++){
-                giroTemp.add(GIRO[i]);
-            }
-            do{
-                int nuevoGiro = aleatorio.nextInt(giroTemp.size());
-                serpiente.penUp();
-                serpiente.turn(giroTemp.get(nuevoGiro));
-                serpiente.move(TRAZO);
-                int posX = serpiente.getPositionX();
-                int posY = serpiente.getPositionY();
-                boolean colision = false;
-                for (int ind = 0; ind < posiciones.size() && !colision; ind++){
-                    SnakePosition temporal = posiciones.get(ind);
-                    if(temporal.getPosX() == posX && temporal.getPosY() == posY 
-                    || posX <= 0 || posY <= 0 || posX >= (INICIO_X * 2) || posY >= (INICIO_Y * 2)){
-                        colision = true;
-                    }
-                }
-                serpiente.moveTo(ultima.getPosX(), ultima.getPosY());
-                serpiente.penDown();
-                if(!colision){
-                    serpiente.move(TRAZO);
-                    int x = serpiente.getPositionX();
-                    int y = serpiente.getPositionY();
-                    posiciones.add(new SnakePosition(x, y));
-                    posiciones.remove(0);
-                    pantalla.erase();
-                    serpiente.penUp();
-                    SnakePosition temporal = posiciones.get(0);
-                    serpiente.moveTo(temporal.getPosX(), temporal.getPosY());
-                    serpiente.penDown();
-                    for (int cont = 1; cont < posiciones.size(); cont++){
-                        temporal = posiciones.get(cont);
-                        serpiente.moveTo(temporal.getPosX(), temporal.getPosY());
-                    }
-                    dibujado = true;
-                }else{
-                    int corregir = giroTemp.get(nuevoGiro);
-                    serpiente.turn( -corregir);
-                    giroTemp.remove(nuevoGiro);
-                }
-            }while(giroTemp.size() > 0 && !dibujado);
-            if(!dibujado){
-                pantalla.setForegroundColor(Color.YELLOW);
-                pantalla.fillRectangle((INICIO_X / 2), (INICIO_Y / 2), INICIO_X, INICIO_Y);
-                pantalla.setForegroundColor(Color.BLACK);
-                pantalla.drawString("Game Over, dude!!!", (int) (INICIO_X * 0.75), INICIO_Y);
-            }
-        }
+        gameDemo.animateSnake();
     }
 }
